fafec76c8315757404c4eb5fdac2e001a978df42
gabcamba <gabcamba@outlook.com>
2025-08-10 13:49:23 +0800

    chore: removed title field

--- db/init.sql
+++ db/init.sql
@@ -1,16 +1,11 @@
 CREATE TABLE IF NOT EXISTS cards (
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
     clicks INT DEFAULT 0,
     first_clicked_at TIMESTAMP
 );
 
-INSERT INTO cards (title) VALUES
-('Card 1'),
-('Card 2'),
-('Card 3'),
-('Card 4'),
-('Card 5'),
-('Card 6'),
-('Card 7'),
-('Card 8');
+INSERT INTO cards (clicks)
+SELECT 0
+FROM generate_series(1, 8);
+
+
--- frontend/src/types/Card.ts
+++ frontend/src/types/Card.ts
@@ -1,6 +1,5 @@
 export type CardData = {
   id: number;
-  title: string;
   clicks: number;
   first_clicked_at: string | null;
 };
